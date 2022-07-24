6828d9cd674a7e565d4afebf9043629b25bf988e
Venkat Sykam <venkatsykam1289@gmail.com>
2022-07-24 00:08:32 -0700

    Finished and uploaded newest version

--- RockPaperScissors/index.js
+++ RockPaperScissors/index.js
@@ -1,59 +1,76 @@
-// function getComputerChoice() {
-//     let num = Math.floor(Math.random() * 3);
-//     if (num == 0) {
-//         return "rock";
-//     } else if (num == 1) {
-//         return "paper";
-//     } else {
-//         return "scissors";
-//     }
-// }
+function getComputerChoice() {
+    let num = Math.floor(Math.random() * 3);
+    if (num == 0) {
+        return "rock";
+    } else if (num == 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
 
-// function getPlayerChoice() {
-//     let choice = prompt("Please enter either Rock, Paper, or Scissors: ").toLowerCase();
-//     return choice;
-// }
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return;
+    }
+    else if (playerSelection == "paper" && computerSelection == "rock" ||
+             playerSelection == "scissors" && computerSelection == "paper" ||
+             playerSelection == "rock" && computerSelection == "scissors") {
+                myScore += 1;
+             }
+    else {
+        computerScore += 1;
+    }
+}
 
-// function playRound(playerSelection, computerSelection) {
-//     if (playerSelection == computerSelection) {
-//         return "It's a tie! Choose again.";
-//     }
-//     else if (playerSelection == "paper" && computerSelection == "rock" ||
-//              playerSelection == "scissors" && computerSelection == "paper" ||
-//              playerSelection == "rock" && computerSelection == "scissors") {
-//                 myScore += 1;
-//                 return `You Win! ${playerSelection} beats ${computerSelection}`;
-//              }
-//     else {
-//         computerScore += 1;
-//         return `You Lose! ${computerSelection} beats ${playerSelection}`;
-//     }
-// }
+function declareWinner() {
+    if (myScore == 5) {
+        alert('Hey you won! Congratulations!')
+    } else {
+        alert('Computer has defeated you, Time to get revenge!')
+    }
+}
 
-// function game() {
-//     let roundCounter = 0;
+function updateScore() {
+    const playerScore = document.getElementById('playerScore');
+    const compScore = document.getElementById('computerScore');
 
-//     while (roundCounter < 5) {
-//         let playerSelection = getPlayerChoice();
-//         let computerSelection = getComputerChoice();
-//         console.log(playRound(playerSelection, computerSelection));
-//         roundCounter += 1;
-//     }   
+    playerScore.textContent = `Your Score: ${myScore}`;
+    compScore.textContent = `Computer Score: ${computerScore}`;
+}
 
-//     console.log(myScore, computerScore);
+let myScore = 0;
+let computerScore = 0;
 
-//     if (myScore == computerScore) {
-//         return "Tie Game!"
-//     }
-//     else if (myScore > computerScore) {
-//         return "You win! Congratulations!"
-//     }
-//     else {
-//         return "You Lost! Better luck next time!"
-//     }
+const options = document.querySelectorAll('.options');
+let result = document.getElementById('result');
 
-// }
+options.forEach((option) => {
+    option.addEventListener('click', function() {
+        const playerSelection = this.value;
+        const computerSelection = getComputerChoice();
+        result.textContent = `You chose: ${playerSelection} | Computer chose: ${computerSelection}`;
+        
+        playRound(playerSelection, computerSelection);
+        updateScore();
+        
+        if (myScore === 5 || computerScore === 5) {
+            declareWinner();
+            result.textContent = '';
+            myScore = 0;
+            computerScore = 0;
+            updateScore();
+        }
+    });
+});
 
-// let myScore = 0;
-// let computerScore = 0;
-// console.log(game());
+const resetBtn = document.getElementById('reset');
+resetBtn.addEventListener('click', function() {
+    const playerScoreText = document.getElementById('playerScore');
+    const computerScoreText = document.getElementById('computerScore');
+    myScore = 0;
+    computerScore = 0;
+
+    playerScoreText.textContent = 'Your Score: 0';
+    computerScoreText.textContent = 'Computer Score: 0';
+});
